329f6892ae35e1ceaccc5f4dd7343ea6dcf3132a
goodtho <Haloreach!!7>
2021-06-17 19:33:08 +0200

    concurrency

--- src/main/java/Concurrency.java
+++ src/main/java/Concurrency.java
@@ -1,3 +1,5 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
@@ -13,5 +15,20 @@ public class Concurrency {
             }
         });
         thread.start();
+    }
+
+    public void threadPool(){
+        // Create the pool
+        ExecutorService service = Executors.newFixedThreadPool(2);
+
+        // submit tasks for execution
+        for(int i = 0; i < 10; i++)
+            service.execute(new Task());
+    }
+}
+
+class Task implements Runnable{
+    public void run(){
+        System.out.println("Thread Name: " + Thread.currentThread().getName());
     }
 }
