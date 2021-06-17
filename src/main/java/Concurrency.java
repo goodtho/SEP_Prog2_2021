567b724173f5d7bb92456b6c6bc4d4a106fc5a02
goodtho <Haloreach!!7>
2021-06-17 17:55:33 +0200

    concurrency

--- src/main/java/Concurrency.java
+++ src/main/java/Concurrency.java
@@ -1,2 +1,17 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
 public class Concurrency {
+
+
+    public void startThread(){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("running");
+                System.out.println("finished");
+            }
+        });
+        thread.start();
+    }
 }
