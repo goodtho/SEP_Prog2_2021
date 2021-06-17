d6994670f445a541769150a3a59b89aaf3b0ddd0
AlainMich <michiala@students.zhaw.ch>
2021-06-17 17:32:17 +0200

    added MVC

---
+++ src/main/java/MVC.java
@@ -0,0 +1,38 @@
+public class MVC {
+    public class Model {
+        private String name;
+        private int prize;
+
+        public Model(String name, int prize) {
+            this.name = name;
+            this.prize = prize;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getPrize() {
+            return prize;
+        }
+    }
+
+    public class View {
+        public void displayItem(String name, int prize) {
+            System.out.println(name + " " + prize);
+        }
+    }
+
+    public class Controller() {
+        private Model model;
+        private View view;
+
+        public Controller(Model model, View view) {
+            this.model = model;
+            this.view = view;
+        }
+        private void printItem() {
+            view.displayItem(model.getName(), model.getPrize());
+        }
+    }
+}
