4dfe04b01b36684229811d00ce96a6ca4768c3a7
AlainMich <michiala@students.zhaw.ch>
2021-06-17 17:33:22 +0200

    added exceptions

--- src/main/java/ErrorHandling.java
+++ src/main/java/ErrorHandling.java
@@ -1,2 +1,40 @@
 public class ErrorHandling {
+    /**
+     * Method that throws an exception
+     * @throws IllegalArgumentException if key is null
+     */
+    public void asdf(String key) {
+        if(key== null) {
+            throw new IllegalArgumentException("key is null");
+        }
+    }
+
+    /**
+     * Call exception throwing method
+     */
+    public void xyz() {
+        try {
+            asdf("1234");
+        } catch(IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Recovery
+     */
+    public void qwer() {
+        boolean succes = false;
+        int attempts = 0;
+        int maxAttempts = 5;
+        do {
+            try {
+                asdf("1234");
+                succes = true;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                attempts++;
+            }
+        } while (!succes && attempts<maxAttempts);
+    }
 }
